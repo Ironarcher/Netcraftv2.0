ac4f2c625a2605bf9c76f12425502bc9567f1bc1
Ironarcher <arpad.kovesdy@gmail.com>
2014-02-05 23:42:01 -0600

    Worked on some stuff including GUI for modules
    MAKE A PACKET HANDLERRRR!

--- src/minecraft/com/nitrogenegames/netcraft/item/ItemUpgrade.java
+++ src/minecraft/com/nitrogenegames/netcraft/item/ItemUpgrade.java
@@ -4,12 +4,19 @@ import com.nitrogenegames.netcraft.Netcraft;
 
 import net.minecraft.item.Item;
 import cpw.mods.fml.relauncher.*;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 
 
 public class ItemUpgrade extends Item {
+	
 public ItemUpgrade(int id) {
 	super(id);
 		setCreativeTab(Netcraft.netcrafttab);
 	}
+
+public void registerIcons(IconRegister par1IconRegister)
+{
+	this.itemIcon = par1IconRegister.registerIcon(Netcraft.modid + ":" + this.getUnlocalizedName());
+}
 }
